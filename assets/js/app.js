cdaf21b02526c48dc799f365101e2f7eb3b78a75
Raymond LOUA <louaraym@gmail.com>
2019-11-18 16:33:04 +0100

    Put back bootswatch using link not yarn

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,6 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import "bootswatch/dist/flatly/bootstrap.min.css";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
